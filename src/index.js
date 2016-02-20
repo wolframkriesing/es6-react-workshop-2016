b2c515795d50fe50eafa5ea40ffdcbea8811a4a5
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-20 23:29:46 +0100

    Render all the group names.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ import ReactDOM from 'react-dom';
 
 class Page extends React.Component {
   render() {
-    return <div>Hi I am react</div>;
+    const katas = this.props.katas;
+    const groupNames = Object.keys(katas.groups);
+    return (
+      <div>
+        <h1>All ES6 katas</h1>
+        {groupNames.map(groupName => <h2>{groupName}</h2>)}
+      </div>
+    );
   }
 }
 
 new GroupedKatas()
   .load(GROUPED_KATAS_URL)
   .then(groupedKatas => {
-    ReactDOM.render(<Page/>, document.getElementById('app'));
+    ReactDOM.render(<Page katas={groupedKatas}/>, document.getElementById('app'));
   });
