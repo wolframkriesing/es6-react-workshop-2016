dedcd199aa0f188bda1cb720fa9b98d7f85e23f7
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-20 23:05:47 +0100

    Make a website, which shows the raw data. (If the browser understood ES6)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import GroupedKatas from './grouped-katas.js';
+import {GROUPED_KATAS_URL} from './config.js';
+
+new GroupedKatas()
+  .load(GROUPED_KATAS_URL)
+  .then(groupedKatas => {
+    document.getElementById('app').innerHTML = JSON.stringify(groupedKatas);
+  });
