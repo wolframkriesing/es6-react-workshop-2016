c303d4bd61a860ae96ff673e33c6499ff967c8a3
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-20 23:12:56 +0100

    Make browserify build our ES6 code, so its useable in the browser.

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import GroupedKatas from './grouped-katas.js';
-import {GROUPED_KATAS_URL} from './config.js';
-
-new GroupedKatas().load(GROUPED_KATAS_URL)
-  .then(groupedKatas => {
-    document.getElementById('app').innerHTML = JSON.stringify(groupedKatas);
-  });
