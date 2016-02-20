9af25d9270481dc1865d21d00db845a6682faaa3
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-20 23:24:55 +0100

    Let's do some react.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import GroupedKatas from './grouped-katas.js';
 import {GROUPED_KATAS_URL} from './config.js';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Page extends React.Component {
+  render() {
+    return <div>Hi I am react</div>;
+  }
+}
+
 new GroupedKatas()
   .load(GROUPED_KATAS_URL)
   .then(groupedKatas => {
-    document.getElementById('app').innerHTML = JSON.stringify(groupedKatas);
+    ReactDOM.render(<Page/>, document.getElementById('app'));
   });
